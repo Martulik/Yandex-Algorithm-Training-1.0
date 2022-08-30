cefa7495ef4e0e75ac4b59d89ffc408fc9beacf9
Marta <ezerinya.m@edu.spbstu.ru>
2022-08-31 02:03:54 +0300

    make colution faster

--- src/contest2/I_Sapper/Sapper.java
+++ src/contest2/I_Sapper/Sapper.java
@@ -3,21 +3,24 @@ package contest2.I_Sapper;
 import java.io.File;
 import java.io.IOException;
 import java.io.PrintStream;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
-import java.util.stream.Stream;
 
 public class Sapper {
     public static void main(String[] args) {
         try (Scanner scanner = new Scanner(new File("src/contest2/I_Sapper/input.txt"));
              PrintStream ps = new PrintStream("src/contest2/I_Sapper/output.txt")) {
-            byte[][] field = new byte[scanner.nextByte()][scanner.nextByte()];
+            byte row = scanner.nextByte();
+            byte col = scanner.nextByte();
             short nMines = scanner.nextShort();
+            List<byte[]> mines = new ArrayList<>(nMines);
             for (int i = 0; i < nMines; ++i) {
                 byte x = scanner.nextByte();
                 byte y = scanner.nextByte();
-                field[x - 1][y - 1] = -1;
+                mines.add(new byte[]{x, y});
             }
-            fillField(field);
+            byte[][] field = getField(mines, row, col);
             printField(field, ps);
         } catch (IOException e) {
             e.printStackTrace();
@@ -26,36 +29,32 @@ public class Sapper {
 
 
     public static void printField(byte[][] field, PrintStream stream) {
-        for (byte[] cell : field) {
-            for (int j = 0; j < cell.length; ++j) {
-                if (cell[j] == -1) {
+        for (int i = 1; i < field.length - 1; ++i) {
+            for (int j = 1; j < field[i].length - 1; ++j) {
+                if (field[i][j] == -1) {
                     stream.print("* ");
                 } else {
-                    stream.print(cell[j] + " ");
+                    stream.print(field[i][j] + " ");
                 }
             }
             stream.println();
         }
     }
 
-    public static void fillField(byte[][] field) {
-        for (int i = 0; i < field.length; ++i) {
-            for (int j = 0; j < field[0].length; ++j) {
-                if (field[i][j] != -1) {
-                    byte left = i != 0 ? field[i - 1][j] : 0;
-                    byte right = i != field.length - 1 ? field[i + 1][j] : 0;
-                    byte below = j != 0 ? field[i][j - 1] : 0;
-                    byte above = j != field[0].length - 1 ? field[i][j + 1] : 0;
-                    byte leftAbove = i != 0 && j != field[0].length - 1 ? field[i - 1][j + 1] : 0;
-                    byte rightAbove = i != field.length - 1 && j != field[0].length - 1 ? field[i + 1][j + 1] : 0;
-                    byte leftBelow = j != 0 && i != 0 ? field[i - 1][j - 1] : 0;
-                    byte rightBelow = j != 0 && i != field.length - 1 ? field[i + 1][j - 1] : 0;
 
-                    field[i][j] = (byte) Stream.of(left, right, below, above, leftAbove, rightAbove, leftBelow, rightBelow)
-                            .filter(x -> x == -1)
-                            .count();
-                }
+    public static byte[][] getField(List<byte[]> mines, byte row, byte col) {
+        byte[][] offset = new byte[][]{{-1, -1}, {-1, 0}, {-1, 1}, {0, -1}, {0, 1}, {1, -1}, {1, 0}, {1, 1}};
+        byte[][] field = new byte[row + 2][col + 2];
+
+        for (int i = 0; i < mines.size(); ++i) {
+            for (int j = 0; j < offset.length; ++j) {
+                ++field[mines.get(i)[0] + offset[j][0]][mines.get(i)[1] + offset[j][1]];
             }
         }
+        for (byte[] mine : mines) {
+            field[mine[0]][mine[1]] = -1;
+        }
+        return field;
     }
 }
+
