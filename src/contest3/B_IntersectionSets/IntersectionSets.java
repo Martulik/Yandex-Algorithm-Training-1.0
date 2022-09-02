c45fddcca1015f0a3a0ae99e6029a51054c59ea7
Marta <ezerinya.m@edu.spbstu.ru>
2022-09-02 23:19:08 +0300

    task2 complete

---
+++ src/contest3/B_IntersectionSets/IntersectionSets.java
@@ -0,0 +1,35 @@
+package contest3.B_IntersectionSets;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.PrintStream;
+import java.util.Arrays;
+import java.util.List;
+import java.util.TreeSet;
+import java.util.stream.Collectors;
+
+public class IntersectionSets {
+
+    public static void main(String[] args) {
+        try (BufferedReader br = new BufferedReader(new FileReader("src/contest3/B_IntersectionSets/input.txt"));
+             PrintStream ps = new PrintStream("src/contest3/B_IntersectionSets/output.txt")) {
+            getIntersectionSets(readList(br), readList(br)).forEach(x -> ps.print(x + " "));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+
+    private static List<Integer> readList(BufferedReader br) throws IOException {
+        return Arrays.stream(br.readLine().split(" "))
+                .map(Integer::parseInt)
+                .collect(Collectors.toList());
+    }
+
+    private static TreeSet<Integer> getIntersectionSets(List<Integer> list1, List<Integer> list2) {
+        TreeSet<Integer> set = new TreeSet<>(list1);
+        set.retainAll(new TreeSet<>(list2));
+        return set;
+    }
+}
