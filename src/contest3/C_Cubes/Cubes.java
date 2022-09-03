5b1b00a099bd44fba35870635b1da6cf166eedd0
Marta <ezerinya.m@edu.spbstu.ru>
2022-09-03 22:40:24 +0300

    make solution faster

--- src/contest3/C_Cubes/Cubes.java
+++ src/contest3/C_Cubes/Cubes.java
@@ -4,51 +4,48 @@ import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
 import java.io.PrintStream;
-import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.List;
-import java.util.stream.Collectors;
+import java.util.TreeSet;
 
 public class Cubes {
     public static void main(String[] args) {
         try (BufferedReader br = new BufferedReader(new FileReader("src/contest3/C_Cubes/input.txt"));
              PrintStream ps = new PrintStream("src/contest3/C_Cubes/output.txt")) {
-            int[] nCubes = Arrays.stream(br.readLine().split("\\s+")).mapToInt(Integer::parseInt).toArray();
-            List<Integer> list1 = readList(br, nCubes[0]);
-            List<Integer> list2 = readList(br, nCubes[1]);
-            List<Integer> list3 = list1.stream().filter(list2::contains).sorted().collect(Collectors.toList());
-            printList(list3, ps);
-            printList(getOtherCubes(list1, list3), ps);
-            printList(getOtherCubes(list2, list3), ps);
+            int[] nCubes = Arrays.stream(br.readLine().trim().split("\\s+")).mapToInt(Integer::parseInt).toArray();
+            TreeSet<Integer> set1 = readTreeSet(br, nCubes[0]);
+            TreeSet<Integer> set2 = readTreeSet(br, nCubes[1]);
+
+            TreeSet<Integer> intersection = (TreeSet<Integer>) set1.clone();
+            intersection.retainAll(set2);
+            printSet(intersection, ps);
+
+            set1.removeAll(intersection);
+            printSet(set1, ps);
+
+            set2.removeAll(intersection);
+            printSet(set2, ps);
+
         } catch (IOException e) {
             e.printStackTrace();
         }
     }
 
-    private static List<Integer> readList(BufferedReader br, int size) throws IOException {
-        List<Integer> list = new ArrayList<>(size);
+    private static TreeSet<Integer> readTreeSet(BufferedReader br, int size) throws IOException {
+        TreeSet<Integer> set = new TreeSet<>();
         for (int i = 0; i < size; ++i) {
-            list.add(readInt(br));
+            set.add(readInt(br));
         }
-        return list;
+        return set;
     }
 
     private static int readInt(BufferedReader reader) throws IOException {
         return Integer.parseInt(reader.readLine());
     }
 
-    private static void printList(List<Integer> list, PrintStream ps) {
-        ps.println(list.size());
-        for (Integer i : list) {
-            ps.print(i + " ");
-        }
+    private static void printSet(TreeSet<Integer> set, PrintStream ps) {
+        ps.println(set.size());
+        set.forEach(x -> ps.print(x + " "));
         ps.println();
     }
 
-    private static List<Integer> getOtherCubes(List<Integer> listWithAllCubes, List<Integer> listCubesToFilter) {
-        return listWithAllCubes.stream()
-                .filter(x -> !listCubesToFilter.contains(x))
-                .sorted()
-                .collect(Collectors.toList());
-    }
 }
